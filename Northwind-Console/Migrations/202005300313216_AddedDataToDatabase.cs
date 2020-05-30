f30ee2ce4e7732006ebb76954edc895e5d898900
asnopek <asnopek486@gmail.com>
2020-05-29 22:46:01 -0500

    Added Migrations to add data to the project, Changed UI to be a switch instead of an if/then statement

---
+++ Northwind-Console/Migrations/202005300313216_AddedDataToDatabase.cs
@@ -0,0 +1,26 @@
+﻿namespace NorthwindConsole.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedDataToDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Products", "ProductName", c => c.String(nullable: false));
+            AlterColumn("dbo.Products", "UnitPrice", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AlterColumn("dbo.Products", "UnitsInStock", c => c.Short(nullable: false));
+            AlterColumn("dbo.Products", "ReorderLevel", c => c.Short(nullable: false));
+            AlterColumn("dbo.Suppliers", "Address", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Suppliers", "Address", c => c.String());
+            AlterColumn("dbo.Products", "ReorderLevel", c => c.Short());
+            AlterColumn("dbo.Products", "UnitsInStock", c => c.Short());
+            AlterColumn("dbo.Products", "UnitPrice", c => c.Decimal(precision: 18, scale: 2));
+            AlterColumn("dbo.Products", "ProductName", c => c.String());
+        }
+    }
+}
--- Northwind-Console/Models/NorthwindContext.cs
+++ Northwind-Console/Models/NorthwindContext.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace NorthwindConsole.Models
 {
     public class NorthwindContext : DbContext
